833c60e44f904ea3b143618ad987f073c5cd8c15
Peter Hulce <122132834+PeterHulce@users.noreply.github.com>
2023-03-16 15:40:43 -0700

    Update pbcscript.js

--- pbcscript.js
+++ pbcscript.js
@@ -13459,7 +13459,7 @@ and limitations under the License.
                     return (
                         p(t, e),
                         (t.prototype.render = function () {
-                            return a.createElement("button", { className: "nav-button pure-button pure-button-primary", onClick: this.props.handleClick, disabled: this.props.isDisabled }, this.props.buttonText);
+                            return a.createElement("button", { className: "button", onClick: this.props.handleClick, disabled: this.props.isDisabled }, this.props.buttonText);
                         }),
                         t
                     );
@@ -14365,7 +14365,7 @@ and limitations under the License.
                             return a.createElement(
                                 "header",
                                 { className: "header-wrapper" },
-                                a.createElement("div", { className: "container" }, a.createElement("h1", { className: "decorated" }, a.createElement("span", { className: "strong" }, e), " Net Price Calculator"))
+                                a.createElement("div", { className: "container" }, a.createElement("h1", " Net Price Calculator"))
                             );
                         }),
                         t
